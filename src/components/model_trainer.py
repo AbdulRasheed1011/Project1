7fad7ebf4868067641843b2f3efe8cb9328da56b
Abdul Rasheed <AbdulRasheed.ax1000@gmail.com>
2024-08-28 18:12:52 -0500

    Hyper Parameters Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -75,17 +75,18 @@ class ModelTrainer:
                 },
                 'Random Forest Regressor': {
                     'n_estimators': [8, 16, 32, 64, 128, 256]
-                }
+                },
+                'K-Neighbors Regressor' : {}
             }
             
 
             model_report: dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, 
-                                                 models=models)
+                                                 models=models, param = params)
 
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
+                list(model_report.values()).index(best_model_score) 
             ]
 
             best_model = models[best_model_name]
